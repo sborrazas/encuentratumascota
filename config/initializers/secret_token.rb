c1930cc0783103796e57f0dcda4d0b1840af11db
Martin Daguerre <mdaguerre@gmail.com>
2012-09-05 15:30:02 -0300

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Encuentratumascota::Application.config.secret_token = 'ff296cbc3210af8b41838a397f9a69374a21dc1b6bd7cdbed6df9b331756a1c33ae46059ac8ee2db1107e47e971820e9dc4614cc1ceec652c4787d1d2b632964'
