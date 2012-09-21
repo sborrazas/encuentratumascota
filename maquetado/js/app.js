0e00a5668ab6bb34f3e64ce5ec3606c7bf694c9f
Ignacio Toledo <ignacio@ign.uy>
2012-09-21 13:16:42 -0300

    JS refactor

--- maquetado/js/app.js
+++ maquetado/js/app.js
@@ -1,82 +1,80 @@
-var Petfinder = (function () {
-    "use strict";
+var PetFinder = function () {
+  this.map = null;
+  this.imageLost = 'img/lost.png';
+  this.imageFound = 'img/found.png';
+  this.imageAdoption = 'img/other.png';
+};
 
-    return {
+PetFinder.prototype.initMap = function () {
+  this.map = new GMaps({
+      div: '#main-panel',
+      lat: -34.90649,
+      lng: -56.16348,
+      zoom: 14
+  });
 
-        map : null,
+  this.populateMarkers();
+};
 
-        initMap: function(){
+PetFinder.prototype.geoLocate = function(){
 
-            this.map = new GMaps({
-                div: '#main-panel',
-                lat: -34.90649,
-                lng: -56.16348,
-                zoom: 14
-            });
+  var self = this;
 
-        this.populateMarkers();
+  GMaps.geolocate({
+    success: function(position) {
+        
+        self.map.setCenter(position.coords.latitude, position.coords.longitude);
+        
+        self.map.addMarker({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            icon: 'http://google-maps-icons.googlecode.com/files/home.png'
+        });
 
-        },
+    },
+    error: function(error) {
+        
+    },
+    not_supported: function() {
+        
+    },
+    always: function() {
+        
+    }
+  });
+};
 
-        geoLocate: function(){
+PetFinder.prototype.populateMarkers = function(){
 
-            GMaps.geolocate({
-                success: function(position) {
-                    
-                    Petfinder.map.setCenter(position.coords.latitude, position.coords.longitude);
-                    
-                    Petfinder.map.addMarker({
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                        icon: 'img/home.png'
-                    });
+  //These should load from backend...
+  var pins = [{lat: -34.90774,  lng: -56.17355},
+              {lat: -34.91170,  lng: -56.16460},
+              {lat: -34.90600,  lng: -56.16190},
+              {lat: -34.92600,  lng: -56.16590},
+              {lat: -34.90600,  lng: -56.16391},
+              {lat: -34.90600,  lng: -56.16392},
+              {lat: -34.914543, lng: -56.167560}];
 
-                },
-                error: function(error) {
-                    
-                },
-                not_supported: function() {
-                    
-                },
-                always: function() {
-                    
-                }
-            });
-        },
+  for (var i = 0; i < pins.length; i++) {
 
-        populateMarkers: function(){
+      this.map.addMarker({
+          lat: pins[i].lat,
+          lng: pins[i].lng,
+          icon: i % 3 === 0 ? this.imageLost : this.imageFound,
+          infoWindow: {
+            content: '<p>HTML Content</p>'
+          }
+      });
+  }
 
-            var pins = [{lat: -34.90774,  lng: -56.17355},
-                        {lat: -34.91170,  lng: -56.16460},
-                        {lat: -34.90600,  lng: -56.16190},
-                        {lat: -34.92600,  lng: -56.16590},
-                        {lat: -34.90600,  lng: -56.16391},
-                        {lat: -34.90600,  lng: -56.16392},
-                        {lat: -34.97600,  lng: -56.16820},
-                        {lat: -34.914543, lng: -56.167560}];
-
-            for (var i = 0; i < pins.length; i++) {
-
-                this.map.addMarker({
-                    lat: pins[i].lat,
-                    lng: pins[i].lng,
-                    icon: i % 3 === 0 ? 'img/lost.png' : 'img/found.png'
-                });
-                
-            }
-
-        }
-    
-
-    }; //end Module
-
-})();
+};
 
 
 $(document).ready(function($) {
 
+  var p = new PetFinder();
 
-        Petfinder.initMap();
-        Petfinder.geoLocate();
+  p.initMap();
+  p.geoLocate();
 
 });
