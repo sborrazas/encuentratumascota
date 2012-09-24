57afbfee33cd106a0470f52f98891b0d91fd5d88
Ignacio Toledo <ignacio@ign.uy>
2012-09-24 16:48:43 -0300

    Minified bootstrap CSS/JS

--- maquetado/js/config.js
+++ maquetado/js/config.js
@@ -4,7 +4,7 @@ require.config({
     jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min',
     async: 'lib/async',
     google_maps_api: 'lib/google_maps_api',
-    bootstrap: 'lib/bootstrap'
+    bootstrap: 'lib/bootstrap.min'
   },
   shim: {
     jquery: { exports: function () { return this.jQuery; } },
