06c9515e6d8a1e2783dceae24d1e302143bd9419
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-02-02 22:39:22 -0200

    Add default breeds and display them by name

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -2,6 +2,8 @@ class Breed < ActiveRecord::Base
 
   has_many :publications
 
+  scope :sorted, order('breeds.name ASC')
+
   def to_s
     name
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # Creating default breeds
-%w(Labrador Beagle Otra).each do |breed_name|
+breeds = File.open(File.join(File.dirname(__FILE__), 'breeds.txt')).read.gsub(/\n/, '').split(',')
+breeds.each do |breed_name|
   puts "Find or create `#{breed_name}`"
   Breed.find_or_create_by_name(breed_name)
 end
