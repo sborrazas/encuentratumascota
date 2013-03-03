5438553fdb4124aaeed524854f0a5ec068d85eb1
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-03-03 14:33:20 -0200

    User gravatar img when user img is not available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module ApplicationHelper
   def title(text, options = {})
     content_for(:title, text)
@@ -20,8 +22,18 @@ module ApplicationHelper
   end
 
   def current_user_image
-    url = current_user.image_url.empty? ? '/assets/default_user.png' : current_user.image_url
-    image_tag url, alt: current_user.display
+    url = if current_user.image_url.empty?
+      if current_user.email.empty?
+        '/assets/default_user.png'
+      else
+        hash = Digest::MD5.hexdigest(current_user.email.downcase)
+        default_img = "#{request.protocol}#{request.host_with_port}/assets/default_user.png"
+        "http://www.gravatar.com/avatar/#{hash}?d=#{u(default_img)}"
+      end
+    else
+      current_user.image_url
+    end
+    image_tag(url, alt: current_user.display)
   end
 
   def publication_type_options
