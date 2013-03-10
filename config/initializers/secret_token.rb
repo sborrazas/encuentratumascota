b3978e86c3dde359cb362872e29b8c3964002a29
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-03-10 14:26:29 -0300

    Hide session secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Encuentratumascota::Application.config.secret_token = 'ff296cbc3210af8b41838a397f9a69374a21dc1b6bd7cdbed6df9b331756a1c33ae46059ac8ee2db1107e47e971820e9dc4614cc1ceec652c4787d1d2b632964'
+Encuentratumascota::Application.config.secret_token = Encuentratumascota::Config::SESSION_SECRET
--- config/settings.rb
+++ config/settings.rb
@@ -3,11 +3,10 @@ require 'rails/all'
 module Encuentratumascota
   module Config
     filename = File.join(File.dirname(__FILE__), 'settings.json')
-    config = if File.exists?(filename)
-      config = ActiveSupport::JSON.decode(File.open(filename).read)
-    else
-      {}
-    end
+
+    raise "`#{filename}` does not exist." unless File.exists?(filename)
+
+    config = ActiveSupport::JSON.decode(File.open(filename).read)
 
     S3_KEY = config['s3_key']
     S3_SECRET = config['s3_secret']
@@ -24,5 +23,7 @@ module Encuentratumascota
     MAIL_HOST = config['mail_host']
     MAIL_DOMAIN = config['mail_domain']
     MAIL_PORT = config['mail_port']
+
+    SESSION_SECRET = config['session_secret']
   end
 end
