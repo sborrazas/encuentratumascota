310bb7e3bd862814e1af7549972a9b3495c2ad75
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-04-29 11:41:03 -0300

    Add assets precompile files list on prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Encuentratumascota::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += Dir[Rails.root.join('app/assets/javascripts/**/*.js')].map {|a| a.gsub(/^.*?javascripts\//, '') }
+  config.assets.precompile += %w(landing.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
