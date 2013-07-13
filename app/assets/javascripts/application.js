91a42bb98f0f2094c025310d57fa5cb05055d761
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-07-13 12:34:42 -0300

    Add countries dropdown on the header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,22 +2,32 @@ define(['jquery'], function ($) {
 
   return {
     init: function () {
-      var dropdown = $('ul.nav-dropdown-menu')
-        , link = dropdown.prev('a')
-        , parent = link.parent();
+      $(".dropdown-nav").each(function (index, dropdown) {
+        var $dropdown = $(dropdown)
+          , $toggler = $dropdown.find(".dropdown-toggle")
+          , $menu = $dropdown.find(".dropdown-list")
+          , activate = function () {
+            $toggler.addClass('active');
+            $menu.show();
+            $menu.width($dropdown.width());
+          };
 
-      dropdown.mouseleave(function (event) {
-        dropdown.hide();
-        link.removeClass('active');
-      });
+        if ($toggler.hasClass("hover-activation")) {
+          $toggler.mouseover(activate);
+        }
+        else {
+          $toggler.click(function (event) {
+            event.preventDefault();
+            activate();
+          });
+        }
 
-      link.click(function (event) {
-        event.preventDefault();
-
-        link.addClass('active');
-        dropdown.show();
-        dropdown.width(parent.width());
+        $menu.mouseleave(function (event) {
+          $menu.hide();
+          $toggler.removeClass('active');
+        });
       });
     }
+
   };
 });
